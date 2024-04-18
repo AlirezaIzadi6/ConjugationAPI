b69cc819c3b494aa2c9f852d643dec13510eddfb
unknown <alirezaizadi199912@gmail.com>
2024-04-18 10:43:13 +0430

    Add ConjugationContext class

---
+++ ConjugationAPI/Models/ConjugationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ConjugationAPI.Models;
+
+public class ConjugationContext : DbContext
+{
+    public DbSet<Conjugation> conjugations { get; set; }
+    public DbSet<Profile> Profiles { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    public ConjugationContext(DbContextOptions<ConjugationContext> options)
+        : base(options)
+    {
+    }
+}
