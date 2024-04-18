58a233fb36af74c1c3b80c8197a2fae308ef6106
unknown <alirezaizadi199912@gmail.com>
2024-04-18 10:12:47 +0430

    Create Profile model

---
+++ ConjugationAPI/Models/Profile.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConjugationAPI.Models;
+
+public class Profile
+{
+    [Key]
+    public int ProfileId { get; set; }
+    public int UserId { get; set; } = 0;
+    public string Name { get; set; } = string.Empty;
+    public string Moods { get; set; } = string.Empty;
+    public string Infinitives {  get; set; } = string.Empty;
+}
