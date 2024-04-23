c2ac23a69cd315d9bfd48499d79c08b227c41ee5
unknown <alirezaizadi199912@gmail.com>
2024-04-23 13:04:16 +0430

    Add question class

---
+++ ConjugationAPI/Models/Question.cs
@@ -0,0 +1,8 @@
+﻿namespace ConjugationAPI.Models;
+
+public class Question
+{
+    public string infinitive { get; set; } = string.Empty;
+    public string Mood { get; set; } = string.Empty;
+    public string person {  get; set; } = string.Empty;
+}
