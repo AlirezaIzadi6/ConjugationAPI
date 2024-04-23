2a3eed2de8afa3af84dc57504987a944e9e8b0c5
unknown <alirezaizadi199912@gmail.com>
2024-04-23 13:13:08 +0430

    Add Answer model to the database

---
+++ ConjugationAPI/Models/Answer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ConjugationAPI.Models;
+
+public class Answer
+{
+    public int Id { get; set; }
+    public string UserId { get; set; } = string.Empty;
+    public string Infinitive { get; set; } = string.Empty;
+    public string Mood { get; set; } = string.Empty;
+    public string Person {  get; set; } = string.Empty;
+    public bool IsCorrect { get; set; } = false;
+}
--- ConjugationAPI/Models/ConjugationContext.cs
+++ ConjugationAPI/Models/ConjugationContext.cs
@@ -6,6 +6,7 @@ public class ConjugationContext : DbContext
 {
     public DbSet<Conjugation> conjugations { get; set; }
     public DbSet<Profile> Profiles { get; set; }
+    public DbSet<Answer> answers { get; set; }
 
     public ConjugationContext(DbContextOptions<ConjugationContext> options)
         : base(options)
