492bcfe6531333b62803b5024a66255234449a92
unknown <alirezaizadi199912@gmail.com>
2024-04-24 14:43:45 +0430

    Add DTO for Question model

---
+++ ConjugationAPI/Models/QuestionDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ConjugationAPI.Models;
+
+public class QuestionDTO
+{
+    public int Id { get; set; } = 0;
+    public string UserId { get; set; } = string.Empty;
+    public string infinitive { get; set; } 
+    public string Mood { get; set; } = string.Empty;
+    public string Person { get; set; } = string.Empty;
+}
