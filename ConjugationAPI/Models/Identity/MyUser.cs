8db1000ff744e2be5f22750cc1d9af83c9306734
unknown <alirezaizadi199912@gmail.com>
2024-04-30 21:27:10 +0430

    Add Score property to MyUser model

--- ConjugationAPI/Models/Identity/MyUser.cs
+++ ConjugationAPI/Models/Identity/MyUser.cs
@@ -5,4 +5,5 @@ namespace ConjugationAPI.Models;
 public class MyUser : IdentityUser
 {
     public DateTime RegistrationDate { get; set; } = DateTime.Now;
+    public int Score { get; set; } = 0;
 }
