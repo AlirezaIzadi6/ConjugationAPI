22f68e9f5c4f946666314003cf842ed5bbc9079e
unknown <alirezaizadi199912@gmail.com>
2024-05-01 13:12:30 +0430

    Add GetDto method to Answer model

--- ConjugationAPI/Models/Conjugation/Answer.cs
+++ ConjugationAPI/Models/Conjugation/Answer.cs
@@ -9,4 +9,13 @@ public class Answer
     public int QuestionId { get; set; } = 0;
     public string AnswerText { get; set; } = string.Empty;
     public DateTime Timestamp { get; set; } = DateTime.Now;
+
+    public AnswerDto GetDto()
+    {
+        return new AnswerDto()
+        {
+            QuestionId = QuestionId,
+            AnswerText = AnswerText
+        };
+    }
 }
