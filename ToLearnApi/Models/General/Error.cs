2388725ec156d6429e5fb2afc68793e6a8234d65
Alireza Izadi <alirezaizadi199912@gmail.com>
2024-05-16 00:33:35 +0430

    Add Error class

---
+++ ToLearnApi/Models/General/Error.cs
@@ -0,0 +1,13 @@
+﻿namespace ToLearnApi.Models.General;
+
+public class Error
+{
+    public string Title { get; set; }
+    public string Message { get; set; }
+
+    public Error(string title, string message)
+    {
+        Title = title;
+        Message = message;
+    }
+}
